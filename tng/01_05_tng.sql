47c4f8990b3988f49fc5548b5bbb45f9fdcf8f2e
YEJIN-GIT <yejinaissi@gmail.com>
2026-04-28 10:21:28 +0900

    260428 [실습] select문 ~ subquery 수정

--- tng/01_05_tng.sql
+++ tng/01_05_tng.sql
@@ -1,7 +1,9 @@
 -- 1. 직급테이블의 모든 정보를 조회해주세요.
 SELECT *
 FROM titles
+ORDER BY title_code
 ;
+
 
 -- 2. 급여가 60,000,000 이하인 사원의 사번을 조회해 주세요.
 SELECT emp_id
@@ -16,6 +18,14 @@ AND emp_id IN(
         AND salaries.emp_id = employees.emp_id
     )
 ;
+-- <teacher>
+SELECT emp_id
+FROM salaries
+WHERE
+    end_at IS NULL
+AND salary <= 60000000
+;
+
 
 -- 3. 급여가 60,000,000에서 70,000,000인 사원의 사번을 조회해 주세요.
 SELECT emp_id
@@ -30,6 +40,14 @@ AND emp_id IN(
         AND salaries.emp_id = employees.emp_id
     )
 ;
+-- <teacher>
+SELECT emp_id
+FROM salaries
+WHERE
+    end_at IS NULL
+AND salary BETWEEN 60000000 AND 70000000
+;
+
 
 -- 4. 사원번호가 10001, 10005인 사원의 사원테이블의 모든 정보를 조회해 주세요.
 SELECT *
@@ -38,6 +56,14 @@ WHERE
     fire_at IS NULL -- 재직 사원
 AND emp_id IN(10001, 10005)
 ;
+-- <teacher>
+SELECT *
+FROM employees
+WHERE 
+    emp_id = 10001
+OR emp_id = 10005
+;
+
 
 -- 5. 직급에 '사'가 포함된 직급코드와 직급명을 조회해 주세요.
 SELECT title_code, title
@@ -45,6 +71,9 @@ FROM titles
 WHERE 
     title LIKE '%사%'
 ;
+-- '_사_' 세글자 중  두번째 `사`
+-- '_사%' 두번재 글자 `사`
+
 
 -- 6. 사원 이름 오름차순으로 정렬해서 조회해 주세요.
 SELECT `name` , emp_id
@@ -53,6 +82,12 @@ WHERE
     fire_at IS NULL -- 재직 사원
 ORDER BY `name`
 ;
+-- <teacher>
+SELECT *
+FROM employees
+ORDER BY `name`, birth
+;
+
 
 -- 7. 사원별 전체 급여의 평균을 조회해 주세요.
 SELECT
@@ -65,6 +100,13 @@ FROM employees
 WHERE
     fire_at IS NULL -- 재직 사원
 ;
+-- <teacher>
+SELECT emp_id
+    ,CEILING(AVG(salary)) avg_sal
+FROM salaries
+GROUP BY emp_id
+;
+
 
 -- 8. 사원별 전체 급여의 평균이 30,000,000 ~ 50,000,000인,
 --   사원번호와 평균급여를 조회해 주세요.
@@ -85,6 +127,14 @@ WHERE
         WHERE fire_at IS NULL
     )
 ;
+-- <teacher>
+SELECT emp_id
+    ,AVG(salary) sal_avg
+FROM salaries
+GROUP BY emp_id
+    HAVING AVG(salary) BETWEEN 30000000 AND 50000000
+;
+
 
 -- 9. 사원별 전체 급여 평균이 70,000,000이상인,
 --   사원의 사번, 이름, 성별을 조회해 주세요.
@@ -101,6 +151,20 @@ AND emp_id IN(
             HAVING AVG(salary) >= 70000000
     )
 ;
+-- <teacher>
+SELECT emp.emp_id
+    ,emp.`name`
+    ,emp.gender
+FROM employees emp
+WHERE 
+    emp_id IN(
+        SELECT emp_id
+        FROM salaries 
+        GROUP BY emp_id
+            HAVING AVG(salary) >= 70000000
+    )
+;
+
 
 -- 10. 현재 직급이 'T005'인,
 --   사원의 사원번호와 이름을 조회해 주세요.
@@ -114,5 +178,18 @@ AND emp_id IN (
         FROM title_emps
         WHERE end_at IS NULL -- 현재 직급
         AND title_code = 'T005'
+    )
+;
+-- <teacher>
+SELECT emp.emp_id
+    ,emp.`name`
+FROM employees emp
+WHERE
+    emp.emp_id IN (
+        SELECT tie.emp_id
+        FROM title_emps tie
+        WHERE
+            tie.end_at IS NULL
+        AND tie.title_code = 'T005'
     )
 ;
